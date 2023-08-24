35428e49ccfda96402d33254dd4d30cf2072ba93
Moscolape <chunkums088@gmail.com>
2023-08-24 17:20:03 +0100

    modify the dashboard sidebar in accordance to given design

--- src/components/my-dashboard/my-dashboard-component.js
+++ src/components/my-dashboard/my-dashboard-component.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState, useContext } from 'react';
 import './my-dashboard-component.styles.css';
 
-import { useNavigate } from 'react-router-dom';
 
+import { useNavigate } from 'react-router-dom';
 
 import TokenContext from '../../contexts/token-context';
 
-import SideBar from '../sidebar-component/sidebar-component';
+import SideBar from '../sidebar/sidebar-component';
 
 const Dashboard = () => {
     const [user, setUser] = useState(null);
     const [crimes, setCrimes] = useState(null);
-    // const [allusers, setAllUsers] = useState(null);
+    const [allusers, setAllUsers] = useState(null);
 
     const { token } = useContext(TokenContext);
-    const [loading, setLoading] = useState(false);
+
+    const [crimesloading, setCrimesLoading] = useState(false);
+    const [userloading, setUserLoading] = useState(false);
 
     const navigate = useNavigate();
 
@@ -46,7 +48,9 @@ const Dashboard = () => {
             .then(data => {
                 console.log("User Data:", data);
                 setUser(data);
-                localStorage.setItem('user', JSON.stringify(data)); // Store user data in localStorage
+                
+                // Store user data in localStorage
+                localStorage.setItem('user', JSON.stringify(data)); 
             })
             .catch(error => {
                 console.error("Error:", error);
@@ -67,7 +71,8 @@ const Dashboard = () => {
     };
 
     const getAllCrimes = () => {
-        setLoading(true);
+        setCrimesLoading(true);
+        console.log('Fetch started');
 
         const apiUrl = "https://crime-analysis-jno2.onrender.com";
         const endpoint = "/api/v1/crimes";
@@ -82,7 +87,7 @@ const Dashboard = () => {
         })
         .then(response => {
             if (!response.ok) {
-                setLoading(false);
+                setCrimesLoading(false);
                 throw new Error(`Request failed with status: ${response.status}`);
             }
             return response.json();
@@ -90,43 +95,43 @@ const Dashboard = () => {
         .then(data => {
             console.log("Crime Data:", data);
             setCrimes(data);
-            setLoading(false);
+            setCrimesLoading(false);
         })
         .catch(error => {
             console.error("Error:", error);
         }); 
     }
 
-    // const getAllUsers = () => {
-    //     setLoading(true);
+    const getAllUsers = () => {
+        setUserLoading(true);
 
-    //     const apiUrl = "https://crime-analysis-jno2.onrender.com";
-    //     const endpoint = "/api/v1/users";
-    //     const url = apiUrl + endpoint;
+        const apiUrl = "https://crime-analysis-jno2.onrender.com";
+        const endpoint = "/api/v1/users";
+        const url = apiUrl + endpoint;
 
-    //     fetch(url, {
-    //         method: "GET",
-    //         headers: {
-    //             "Authorization": `Bearer ${token}`,
-    //             "Content-Type": "application/json"
-    //         }
-    //     })
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             setLoading(false);
-    //             throw new Error(`Request failed with status: ${response.status}`);
-    //         }
-    //         return response.json();
-    //     })
-    //     .then(data => {
-    //         console.log("All Users Info:", data);
-    //         setAllUsers(data);
-    //         setLoading(false);
-    //     })
-    //     .catch(error => {
-    //         console.error("Error:", error);
-    //     }); 
-    // }
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                setUserLoading(false);
+                throw new Error(`Request failed with status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("All Users Info:", data);
+            setAllUsers(data);
+            setUserLoading(false);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        }); 
+    }
 
 
     return (
@@ -146,7 +151,7 @@ const Dashboard = () => {
                 </div>
                 <div className='crimes'>
                     <button id='get-crimes' onClick={getAllCrimes} title='Load the crime report'>
-                        {loading ? "Loading all crimes..." : "Check crimes"}
+                        {crimesloading ? "Loading..." : "Check crimes"}
                     </button>
                     <div>
                         {crimes ? (
@@ -155,25 +160,28 @@ const Dashboard = () => {
                                 <p>There are {crimes.count} crime reports in the provided data.</p>
                             </div>
                         ) : (
-                            <p>No data has been displayed yet...</p>
+                            <p>{crimesloading ? "Getting all crime data..." : "No crime data yet."}</p>
                         )}  
                     </div>
                 </div>
-                {/* <div className='users'>
+                <div className='users'>
                     <button id='get-crimes' onClick={getAllUsers} title='Load the crime report'>
-                        {loading ? "Loading all users..." : "Check all users"}
+                        {userloading ? "Loading..." : "Check all users"}
                     </button>
                     <div>
                         {allusers ? (
                             <div>
                                 <h2>All Users</h2>
-                                <p>{allusers.msg[0].email}</p>
+                                {/* <p>{allusers.msg[0].email}</p> */}
+                                {allusers.msg.map(user => {
+                                    return <li>{user.fullName}</li>
+                                })}
                             </div>
                         ) : (
-                            <p>No user has been displayed yet...</p>
+                            <p>{userloading ? "Getting all user data..." : "No user data yet."}</p>
                         )}  
                     </div>
-                </div> */}
+                </div>
             </main>
         </section>
     );
---
+++ src/components/sidebar/sidebar-component.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './sidebar-component.styles.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faHome, faNewspaper, faBell, faGear, faChartLine } from '@fortawesome/free-solid-svg-icons';
+
+
+import navImage from '../../assets/user.png';
+
+const SideBar = (props) => {
+    const user = props.user;
+    const lastName = user && user.data.fullName.split(' ').pop();
+
+    const onLogout = props.onLogout;
+
+    return (
+        <div className="sidebar">
+            {user ? (
+                <div>
+                    <div className='side-nav'>
+                        <img src={navImage} alt='user-pic' id='nav-pic'/>
+                        <div>
+                            <span>CRIME FORECASTING AND ANALYSIS</span>
+                            <p>Welcome, <b>{lastName}</b></p>
+                        </div>
+                    </div>
+                    <div className='nav-input'>
+                        <input type='search' className='nav-search-input'/>
+                        <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+                    </div>
+                    <div className='nav-menu'>
+                        <p style={{color: '#997BD1', fontWeight: 'bold'}}>Menu</p>
+                        <div>
+                            <FontAwesomeIcon icon={faHome} className='nav-icon'/> <span>Home</span>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faNewspaper} className='nav-icon'/> <span>News Reports</span>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faChartLine} className='nav-icon'/> <span>Context Analysis</span>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faBell} className='nav-icon'/> <span>Notifications</span>
+                        </div>
+                        <div>
+                            <FontAwesomeIcon icon={faGear} className='nav-icon'/> <span>Settings</span>
+                        </div>
+                    </div>
+                    <button id='btn-bottom' onClick={onLogout} title='Log out'>Logout</button>
+                </div>
+            ) : (
+                <p>Loading user...</p>
+            )}
+        </div>
+    );
+};
+
+export default SideBar;
